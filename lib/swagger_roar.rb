1ec5aa58639347d49ab405b7b3ac45b7d528f386
Yura Taras <yura.taras@gmail.com>
2014-07-01 21:03:00 +0300

    Fixed test for 1.9.3

--- lib/swagger_roar.rb
+++ lib/swagger_roar.rb
@@ -2,9 +2,22 @@ require "swagger_roar/version"
 
 module SwaggerRoar
   def documentation
-    attrs = representable_attrs.to_h.map { |k, v|
-      [k.to_sym, v[:documentation]]
-    }
-    Hash[attrs]
+    Hash[
+      attrs_hash.map { |k, v|
+        [k.to_sym, v[:documentation]]
+      }
+    ]
+  end
+
+  private
+
+  def attrs_hash
+    a = representable_attrs
+    return a.to_h if a.respond_to? :to_h
+    Hash[
+      a.keys.map { |k|
+        [k.to_sym, a[k]]
+      }
+    ]
   end
 end
